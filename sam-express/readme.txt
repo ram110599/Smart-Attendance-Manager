835e02ddd7ae92fb5c88ca1378167eea030ebd3c
sahilk313 <sahil.k.313999@gmail.com>
2020-02-19 05:18:36 +0530

    created admin login

--- sam-express/readme.txt
+++ sam-express/readme.txt
@@ -4,7 +4,7 @@ We are using node.js as well as epress framework.
 ============
 
 set port to 8000 in bin/www line 15.
-how to run: go to sam-epress. Type "npm start" in powershell.
+how to run: go to sam-epress. Type "npm start" in powershell. in browser, type "localhost:8000"
 Imp files for modification: app.js, routes/index.js, views/layouts/*
 package.json has list of reuirements of the project
 
--- sam-express/routes/index.js
+++ sam-express/routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', success: req.session.success, errors: req.session.errors });
+  //if session is success means already logged in
+  //else reset errors and success
   if(req.session.success !== true){
     req.session.errors = null;
     req.session.success = null;
@@ -11,35 +13,36 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/logout', function(req, res, next){
+	//to logout reset success value and redirect to home page
 	req.session.success = false;
 	res.redirect('/');
 });
 
-router.get('/test/:param1/:param2', function(req, res, next){
-	res.render('test', {output1: req.params.param1, output2: req.params.param2});
-});
 
 router.post('/submit', function(req, res, next){
 	//validation
 	req.check('id', 'No ID entered').isLength({min: 1}); //this checks the 'id' named parameter
 	req.check('password', 'No password entered').isLength({min: 1});
 	var errors = req.validationErrors();
+	//if errors exist then not validated. hence, sent back to login page
 	if (errors){
 		req.session.errors = errors;
 		req.session.success = false;
 		res.redirect('/');
 	}
 	else{
-		//verification
+		//verification after validation
 		var id = req.body.id;
 		var password = req.body.password;
 		var sql = "SELECT * from admin_login where `id`='"+id+"' and `password`='"+password+"'";
 		db.query(sql, function(err, results){
+			//correct userid and password
 			if(results.length){
 				req.session.id = results[0].id;
 				req.session.success = true;
 				res.redirect('/');
 			}
+			//incorrect userid or password
 			else{
 				req.session.success = false;
 				var error = {param: "id", msg: "Invalid user id or password", value: req.body.id};
